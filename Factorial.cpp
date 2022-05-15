4d1187712a7ecb9859790c696899020a67ebb8c2
樋口 <higuchis662@gmail.com>
2022-05-16 00:46:00 +0900

    階乗

---
+++ Factorial.cpp
@@ -0,0 +1,11 @@
+
+int Factorial(int n)
+{
+	if (n == 0)
+	{
+		return 1;
+	}else
+	{
+		return  n * Factorial(n - 1);
+	}
+}
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Algorithm.h"
+
+using namespace std;
+
+int main()
+{
+	cout << Factorial(4);
+}
